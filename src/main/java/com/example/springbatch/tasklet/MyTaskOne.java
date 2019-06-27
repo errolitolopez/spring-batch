440f3f0c7b069085a2314de8b295ca575b925261
Errolito Lopez <errolito.lopez@whitecloak.com>
2019-06-27 10:46:07 +0800

    Initial commit

---
+++ src/main/java/com/example/springbatch/tasklet/MyTaskOne.java
@@ -0,0 +1,17 @@
+package com.example.springbatch.tasklet;
+
+import org.springframework.batch.core.StepContribution;
+import org.springframework.batch.core.scope.context.ChunkContext;
+import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.repeat.RepeatStatus;
+
+public class MyTaskOne implements Tasklet {
+
+    @Override
+    public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
+
+        System.out.println("MyTaskOne Start..");
+        System.out.println("MyTaskOne Done..");
+        return RepeatStatus.FINISHED;
+    }
+}
